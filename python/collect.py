006f2ef2082511675f6ecc71bcd715acbe9a80cd
sfyyes92 <112747668+sfyyes92@users.noreply.github.com>
2025-04-26 15:07:58 +0800

    Update collect.py
    change the finditer

--- python/collect.py
+++ python/collect.py
@@ -75,7 +75,8 @@ def brute_force_password(encrypted_data):
 
 def parse_urls(decrypted_data):
     """Parses URLs from the decrypted data."""
-    matches = re.finditer(r"http.*\.(yaml|txt)", decrypted_data)
+    # matches = re.finditer(r"http.*\.(yaml|txt)", decrypted_data)
+    matches = re.finditer(r"https?://[^\s()<>]+(?:\([^\s()<>]+\)|[^.,;:\s<>\[\]()])+\.(?:txt|yaml)", decrypted_data)
     urls = [match.group(0) for match in matches]
     if not urls:
         raise ValueError("No URLs found in the decrypted data.")
